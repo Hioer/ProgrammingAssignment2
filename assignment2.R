2c8a5dfa57cd8dde1116444285cca80ccddf5160
Han Wang <wanghan1227@gmail.com>
2019-09-16 01:28:19 -0400

    my assignment of coursera

---
+++ assignment2.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inverse) inv <<- inverse
+    getInv<- function() inv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
+}
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getInv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInv(inv)
+    inv
+}
